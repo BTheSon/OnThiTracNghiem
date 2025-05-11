8ab03acaff2349eaf701aaa7c209c19cabf50d2f
Bui_The_Son <buitheson201@gmail.com>
2025-05-11 20:43:43 +0700

    sửa lại controller và sửa lại chút animation cho card

--- app/controllers/TestView.php
+++ app/controllers/TestView.php
@@ -3,7 +3,10 @@ namespace App\Controllers;
 use App\Core\Controller;
 
 class TestView extends Controller{
-    public function t(string $viewName) : void {
+    public function t(string  ...$viewName) : void {
+        // chuyển đổi các tham số thành chuỗi
+        $viewName = implode('/', $viewName);    
+
         $this->view($viewName);
     }
 }
--- app/views/pages/pageGV/trangchugv.php
+++ app/views/pages/pageGV/trangchugv.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="<?=BASE_URL?>/public/css/">
-    <link rel="stylesheet" type="text/css" href="-----"></head>
+    <base href="<?=BASE_URL?>/">
+    <link rel="stylesheet" type="text/css" href="public/css/trangchugv.css"></head>
     <title>Giáo Viên</title>
 </head>
 <body>
