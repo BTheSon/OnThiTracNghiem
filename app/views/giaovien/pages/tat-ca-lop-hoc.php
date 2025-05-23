cde19cb3a242ada3e33c467f59f5e083e2b4200d
tuwns05 <tuantran290705@gmail.com>
2025-05-23 09:32:51 +0700

    design menu add trong qlLop

--- app/views/giaovien/pages/tat-ca-lop-hoc.php
+++ app/views/giaovien/pages/tat-ca-lop-hoc.php
@@ -3,6 +3,7 @@
     <div class="class-cards">
         <?php foreach($data['info_classes'] as $classes): ?>
             <a href="<?=$classes['id']?>" class="card">
+                <div class="card-mota"><?=$classes['mo_ta']?></div>
                 <div class="card-header"><?=$classes['ten_lop']?></div>
             </a>
         <?php endforeach; ?>
