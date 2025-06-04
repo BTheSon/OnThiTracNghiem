45ad18cad51bee11273bfe7fc54d66816eddadab
Bui_The_Son <buitheson201@gmail.com>
2025-06-04 21:21:44 +0700

    fix: tự động lấy sub dir (không còn bị hard code như trước)

--- config/constants.php
+++ config/constants.php
@@ -1,35 +1,39 @@
 <?php
 
+$docRoot = realpath($_SERVER['DOCUMENT_ROOT']);         // /var/www
+$projectDir = BASE_DIR;                         // /www//onthitracnghiem/
+
+$subFolder = str_replace($docRoot, '', $projectDir);     // /2022/big_assignment/onthitracnghiem
+$subFolder = str_replace('\\', '/', $subFolder);         // Windows compatible
+
+// tên folder con
+define('SUB_DIR_NAME', rtrim($subFolder, '/'));
+
+/**
+ * URL gốc của ứng dụng (localhost/OnThiTracNghiem)
+ * Đường dẫn này sẽ được sử dụng để tạo các liên kết trong ứng dụng
+ */
+define('BASE_URL', (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . SUB_DIR_NAME);
+
 // Tên của ứng dụng
-define('APP_NAME', 'MindQuest');
+define('APP_NAME', 'QUIZ');
 
-// tên folder con mà bạn đã đặt tên trong dự án
-define('SUB_DIR_NAME', '/OnThiTracNghiem');
-
-// Thư mục gốc của dự án, được tính toán từ vị trí của file 'config'
-define('BASE_DIR', dirname(__DIR__));
-
-// Thư mục chứa các file view của ứng dụng
+/* Thư mục chứa các file view của ứng dụng*/
 define('BASE_VIEWS_DIR', BASE_DIR . '/app/views');
 
-// Thư mục chứa các file model của ứng dụng
+/* Thư mục chứa các file model của ứng dụng */
 define('BASE_MODELS_DIR', BASE_DIR . '/app/models');
 
-// Thư mục chứa các file controller của ứng dụng
+/**Thư mục chứa các file controller của ứng dụng*/
 define('BASE_CONTROLLERS_DIR', BASE_DIR . '/app/controllers');
 
-// URL gốc của ứng dụng (localhost/OnThiTracNghiem)
-// Đường dẫn này sẽ được sử dụng để tạo các liên kết trong ứng dụng 
-define('BASE_URL', (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . SUB_DIR_NAME);
-
-/** đường dấn tương đối với thư mục dự án */
+/** đường dấn tương đối với thư mục chứa các file lưu trữ trong dự án */
 define('RELATIVE_STORAGE_PATH', '/storages');
 
 /*** Đường dẫn tới folder chứa các file lưu trữ như hình ảnh, tài liệu */
 define('BASE_STORAGE_DIR', BASE_DIR . RELATIVE_STORAGE_PATH);
 
-
-/** định nghĩa các type của file mà giáo viên có thể tải lên */
+/** định nghĩa các đuôi mở rộng của file mà giáo viên có thể tải lên */
 define('ALLOWED_FILE_TYPES', [
     'pdf',
     'doc',
@@ -41,4 +45,5 @@ define('ALLOWED_FILE_TYPES', [
     'png'
 ]);
 
+/**đường dẫn tới ảnh đại diện mặc định của người dùng */
 define('DEFAULT_AVT_URL', 'public/image/avt.png');
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 use App\Core\App;
 session_start();
 
+define('BASE_DIR', realpath(__DIR__)); // Thư mục gốc của dự án
+
 require_once './config/constants.php';
 require_once './config/DBconfig.php';
 require_once BASE_DIR . '/app/includes/ErrorDisplay.php';
